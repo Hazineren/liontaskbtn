227a2de09f30569d9e549126b101c6843ea98f76
Hazineren <hazineren24@gmail.com>
2023-04-14 02:15:26 +0300

    React-Publishing-task

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
-import { ExampleComponent } from 'liontaskbtn'
+import { ExampleComponent, Button } from 'liontaskbtn'
 import 'liontaskbtn/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+    <div className='container'>
+      <Button type='primary' text='Primary Button'></Button>
+      <Button type='Default Button' text='Default Button'></Button>
+      <Button type='dashed' text='Dashed Button'></Button>
+      <Button type='text' text='Text Button'></Button>
+      <Button type='link' text='Link Button'></Button>
+      </div>
+    </>
+  )
 }
 
 export default App
